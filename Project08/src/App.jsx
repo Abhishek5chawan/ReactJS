a7cc32a8446fe1a4a6a3af9a9be977a712f90632
Abhishek C <abhishekchawan907@gmail.com>
2024-08-16 10:45:44 +0530

    Add files via upload

---
+++ Project08/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './index.css';
+import AddTodo from './Components/AddTodo';
+import Todo from './Components/Todo';
+
+function App() {
+ 
+  return (
+  <>
+  <AddTodo/>
+  <Todo/>
+  </>
+);
+}
+
+export default App
